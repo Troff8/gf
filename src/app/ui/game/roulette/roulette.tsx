4bb48f4d6627f7351441871401c069f2aa5cd2d6
Troff <troffibp@gmail.com>
2024-03-29 15:00:13 +0300

    draft roulette

--- src/app/ui/game/roulette/roulette.tsx
+++ src/app/ui/game/roulette/roulette.tsx
@@ -1,38 +1,125 @@
 "use client";
-
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
 import styles from "./roulette.module.css";
+import clsx from "clsx";
+import { GiFrogPrince } from "react-icons/gi";
 
-const players: any[] = [];
-for (let i = 0; i < 100; i++) {
-  players.push(i);
-}
+const cells = 31;
+
+const items = [
+  { name: "gf", img: "/avatar.jpg", chance: 99 },
+  { name: "jesse", img: "/jesse.jpg", chance: 1 },
+  { name: "walter", img: "/walterWhite.png", chance: 0 },
+  { name: "soul", img: "/soul.jpeg", chance: 0 },
+];
+
 const Roulette = () => {
-  const ulRef = useRef(null);
-  const start = () => {
-    const move = -150 * 15 * Math.floor(Math.random() * 2);
-    const ulElement = ulRef.current;
+  const [isStarted, setIsStarted] = useState(false);
+  const [selectedItemIndex, setSelectedItemIndex] = useState<number>();
+  const [players, setPlayers] = useState([]);
+  let isFirstStart = true;
 
-    if (ulElement) {
-      ulElement.style.left = move + "px";
+  function getItem() {
+    let item;
+
+    while (!item) {
+      const chance = Math.floor(Math.random() * 100);
+
+      items.forEach((elm) => {
+        if (chance < elm.chance && !item) item = elm;
+      });
     }
+
+    return item;
+  }
+
+  function generateItems() {
+    for (let i = 0; i < cells; i++) {
+      const item = getItem();
+      setPlayers([...players, item]);
+    }
+  }
+
+  const start = () => {
+    if (isStarted) return;
+    setIsStarted(true);
+    if (!isFirstStart) generateItems();
+    isFirstStart = false;
   };
+
+  useEffect(() => {
+    if (isStarted) {
+      const interval = setInterval(() => {
+        const index = Math.floor(Math.random() * cells);
+        setSelectedItemIndex(index);
+      }, 100);
+
+      setTimeout(() => {
+        setIsStarted(false);
+        clearInterval(interval);
+      }, 5000); // Transition time
+
+      return () => clearInterval(interval);
+    }
+  }, [isStarted]);
 
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.scopeHidden}>
-          <ul ref={ulRef}>
-            {players.map((player) => (
-              <li key={player}>{player}</li>
-            ))}
+      <div className={styles.game}>Game #12312412</div>
+      <div className={styles.roulette}>
+        <div className={styles.triangle}></div>
+        <div className={styles.scope}>
+          <ul
+            className={clsx(styles.list, {
+              [styles.isStoped]: !isStarted,
+              [styles.isStarted]: isStarted,
+            })}
+          >
+            {[...Array(cells)].map((_, index) => {
+              const item = items[index % items.length];
+              return (
+                <li
+                  key={index}
+                  className={clsx(styles.list__item, {
+                    [styles.active]: index === selectedItemIndex,
+                  })}
+                >
+                  <Image
+                    src={item.img}
+                    alt={item.name}
+                    width={100}
+                    height={100}
+                    className={styles.image}
+                  />
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
-
-      <div className={styles.triangle}></div>
-      <button onClick={start}>Go</button>
+      <div className={styles.game}>
+        <div className={styles.information}>
+          <div>
+            Winner: <span className={styles.nicknameWinner}>???</span>
+          </div>
+          <div className={styles.bet}>
+            <GiFrogPrince size={20} color="yellow" />
+            <div>104343</div>
+          </div>
+        </div>
+        <div className={styles.actions}>
+          <div>
+            Min bet 2 <GiFrogPrince size={15} color="yellow" />
+            <div> The higher the bet, the greater the chance to win.</div>
+          </div>
+          <button onClick={start} className={styles.start}>
+            Place a bet
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Roulette;
